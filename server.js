bc1b6f83123898aaaaa911c54660d02c538ffaf0
Pankaj Lal <lalpankaj0@gmail.com>
2020-09-08 12:29:54 +0530

    Final Commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+// const server = require('http').Server(app)
+const server = app.listen(3000);
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+const { v4: uuidV4 } = require('uuid')
+
+
+
+app.use(express.static('public'));
+app.set('view engine', 'twig');
+app.set('views','./public/views');
+
+app.use('/peerjs', peerServer);
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
